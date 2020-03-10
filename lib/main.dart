c4384d35a33601447c9234a8fbd24136d533275b
jdarangop <juandiegoap33@gmail.com>
2020-03-10 14:09:12 -0500

    add a test to handle the scopes error in speech to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:franle_app/chat/chatfranle.dart';
 import 'package:franle_app/web_socket/websocket.dart';
 import './chat/franlechat.dart';
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChatFranle(), //WebSocketChat(),
+      home: ChatFran(),//ChatFranle(), WebSocketChat(),
     );
   }
 }
